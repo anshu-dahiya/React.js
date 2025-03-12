aa6c494fa8471f67b25ddc9be47f33de0b2e1009
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-03-12 14:42:10 +0530

    Pagination

--- practicproblems/src/Fifteen.js
+++ practicproblems/src/Fifteen.js
@@ -9,13 +9,26 @@ export default function Fifteen({ items, itemsPerPage }) {
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = items.slice(indexOfFirstItem,indexOfLastItem);
 
-    console.log(indexOfLastItem);
-    console.log(indexOfFirstItem);
-    console.log(currentItems);
+    const pageNumbers = [];
+    for(let i=1; i<=Math.ceil(items.length / itemsPerPage); i++){
+        pageNumbers.push(i);
+    }
 
     return (
         <div>
+            <ul>
+                {currentItems.map((item, index) => (
+                    <li key={index}>
+                        {item}
+                    </li>
+                ))}
+            </ul>
 
+            {pageNumbers.map((number) => (
+                <li key={number} onClick={() => setCurrentPage(number)}>
+                    {number}
+                </li>
+            ))}
         </div>
     )
 }

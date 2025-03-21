5424183d2f9a66217b57aec76f9b77b9994dc18c
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-03-21 23:41:23 +0530

    useContext

---
+++ practicproblems/src/Eighteen.js
@@ -0,0 +1,11 @@
+//Context provider that allows user to switch between Light and Dark theme
+//and use context in diff. parts of the app to apply Selected theme
+import React from "react";
+
+export default function Eighteen(){
+    return (
+        <div>
+            
+        </div>
+    )
+}

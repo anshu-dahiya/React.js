a54b33d29cda7218a7cb2ab7035defc8f1e6a391
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-03-23 19:09:30 +0530

    learn components and props use

--- practicproblems/src/Nineteen.js
+++ practicproblems/src/Nineteen.js
@@ -1,55 +1,22 @@
-//Card component 
+//Creating Card Component to use it multiple time with the help of props
 import React from 'react'
 import NineteenList from './NineteenList'
+import mobiles from './NineteenData.json'
 
 export default function Nineteen() {
 
-  const mobile1 = { 
-    image : 'https://m.media-amazon.com/images/I/51izGKpbXOL._SY679_.jpg',
-    title : 'Nothing Phone 3A',
-    price : '1,200'
-  }
-  const mobile2 = { 
-    image : 'https://m.media-amazon.com/images/I/61EscGf8mYL._SX679_.jpg',
-    title : 'IPhone 16 Plus',
-    price : '10,000'
-  }
-  const mobile3 = { 
-    image : 'https://m.media-amazon.com/images/I/41P2s-83N8L._SX300_SY300_QL70_FMwebp_.jpg',
-    title : 'Redmi Note 14',
-    price : '700'
-  }
-  const mobile4 = { 
-    image : 'https://m.media-amazon.com/images/I/4140OGrNrgL._SX300_SY300_QL70_FMwebp_.jpg',
-    title : 'Samsung Galaxy A56',
-    price : '8,500'
-  }
-
   return (
     <div>
-      <NineteenList 
-      image={mobile1.image} 
-      title={mobile1.title}
-      price={mobile1.price}
-      />
-      
-      <NineteenList 
-      image={mobile2.image} 
-      title={mobile2.title}
-      price={mobile2.price}
-      />
-      
-      <NineteenList 
-      image={mobile3.image} 
-      title={mobile3.title}
-      price={mobile3.price}
-      />
-      
-      <NineteenList 
-      image={mobile4.image} 
-      title={mobile4.title}
-      price={mobile4.price}
-      />
+
+      {mobiles.map((element) => {
+        
+        return <NineteenList 
+        image={element.image} 
+        title={element.title}
+        price={element.price}
+        />
+      })}
+
     </div>
   )
 }
--- practicproblems/src/NineteenList.js
+++ practicproblems/src/NineteenList.js
@@ -3,14 +3,15 @@ import './styles/Nineteen.css'
 
 export default function NineteenList(props) {
 
+  const {image,title,price} = props;
 
   return (
     <div className='main'>
         {console.log(props)}
-        <img className='image' src={props.image}/>
+        <img className='image' src={image}/>
         <div>
-        <h2>{props.title}</h2>
-        <p>$ {props.price}</p>
+        <h2>{title}</h2>
+        <p>$ {price}</p>
         <button>Add to cart</button>
         </div>
     </div>

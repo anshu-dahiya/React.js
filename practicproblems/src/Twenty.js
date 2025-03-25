4b35acf0626ca9f56a8c4faa27c738713d697900
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-03-26 00:16:45 +0530

    Add,Remove,Clear,Update a data from list by useState

--- practicproblems/src/Twenty.js
+++ practicproblems/src/Twenty.js
@@ -1,3 +1,4 @@
+//Add,Remove,Clear,Update a data from list by useState
 import React, { useState } from 'react'
 import data from './NineteenData.json'
 
@@ -9,6 +10,26 @@ const handleClear = () => {
     setnData([])
 }
 
+const handleRemove = (itemRemoved) => {
+    setnData(nData.filter(item => 
+        item !== itemRemoved))
+        //The arrow function checks each item
+        //And keeps items where the item is NOT equal to itemRemoved
+}
+
+const handleUpdate = (itemUpdated) => {
+    setnData(nData.map(item => {
+        if(item === itemUpdated){
+            const update = <input type='text' placeholder='Enter your new update'/>
+            return {title:update}       
+        }
+        else{
+            return item;
+        }
+    }
+    ))
+}
+
   return (
     <div>
         <ol>
@@ -16,6 +37,9 @@ const handleClear = () => {
             <h2>
                 <li>
                     {items.title}
+                    <br/>
+                    <button onClick={() => handleUpdate(items)}>Update</button>
+                    <button onClick={() => handleRemove(items)}>Remove</button>
                 </li>
             </h2>
             )} 

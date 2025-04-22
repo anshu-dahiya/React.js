d7ef883801c092e267e2d80a3029f3e036991c62
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-04-22 19:56:38 +0530

    CRUD operation in rtk-query

--- rtk-query/src/App.js
+++ rtk-query/src/App.js
@@ -1,11 +1,90 @@
 import React from 'react'
+import { 
+  useCreatePostMutation, 
+  useDeletePostMutation, 
+  useGetApiByNameQuery, 
+  useUpdatePostMutation } from './services/api'
 
-const App = () => {
+export default function App() {
+  const {
+    data,
+    error,
+    isLoading,
+    isSuccess,
+    isFetching} = useGetApiByNameQuery();
+
   return (
     <div>
-      
+      <h1>React RTK Query</h1>
+      {isLoading && <h2>Loding....</h2>}
+      {isFetching && <h2>Fetching....</h2>}
+      {error && <h2>Error</h2>}
+      {isSuccess && (
+        <div>
+          {data?.map(posts => {
+              return(
+                <div key={posts.id}>
+                  <span>{posts.title}</span>
+                </div>
+              )  
+          })}
+        </div>
+      )}
+
+      <ChangePost/>
+
     </div>
   )
 }
 
-export default App
+export const ChangePost = () => {
+  const [addPost] = useCreatePostMutation();
+  const [deletePost] = useDeletePostMutation();
+  const [updatePost] = useUpdatePostMutation();
+  
+  const posts = [
+    {
+    "id":4,
+    "title":"Post 4 Posted",
+    "author":"Me",
+    },
+    {
+    "id":2,
+    "title":"Post 2 Posted",
+    "author":"Me"
+    },
+    {
+    "id":3,
+    "title":"Post 3 Posted",
+    "author":"Me"
+    },
+  ]
+  
+  const postUpdate = {
+    "id":3,
+    "title":"Post 3 Updated",
+    "author":"Me Updated"
+  }
+
+  const handlePost = async() => {
+    for(let post of posts) {
+      await addPost(post);
+    }
+  }
+   
+  const handleDelete = async() => {
+    await deletePost(2);
+  }
+  
+  const handleUpdate = async() => {
+    await updatePost(postUpdate);
+  }
+
+  return(
+    <>
+      <button onClick={handlePost}>Add Post</button>;
+      <button onClick={handleDelete}>Delete Post</button>;
+      <button onClick={handleUpdate}>Update Post</button>;
+    </>
+  )
+}
